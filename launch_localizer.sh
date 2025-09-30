8f87832de73ceb12d41cbb54cd9ab18225004105
Alan Papalia <alanpapalia@gmail.com>
2025-09-30 11:07:52 -0400

    cleanup

--- launch_localizer.sh
+++
@@ -1,4 +0,0 @@
-conda activate nail-utils
-LD_PRELOAD="$CONDA_PREFIX/lib/libstdc++.so.6" \
-LD_LIBRARY_PATH="$CONDA_PREFIX/lib:${LD_LIBRARY_PATH}" \
-"$CONDA_PREFIX/bin/python" /home/alan/nail-experiment-utils/pointcloud_localizer_gui.py
